d491734b796eabb4e1a29256cdafed46e75da494
Yvan <yfyvan@gmail.com>
2020-05-19 21:37:31 +0800

    ⬆️resize

--- test.py
+++ test.py
@@ -6,11 +6,14 @@ img_path = '/Users/robbe/others/tf_data/voc2007/images/000030.jpg'
 img = cv.imread(img_path)
 boxes = [[36, 205, 180, 289], [51, 160, 150, 292], [295, 138, 450, 290]]
 a = Augment()
+
+new_image = cv.resize(img, (200, 400), interpolation=cv.INTER_CUBIC)
+
 #
-new_image, new_boxes = a.mosaic(img, boxes)
-cv.imshow('', new_image)
-cv.waitKey()
-cv.destroyAllWindows()
+a.resize(img)
+# cv.imshow('', new_image)
+# cv.waitKey()
+# cv.destroyAllWindows()
 
 
 # for box in new_boxes:
